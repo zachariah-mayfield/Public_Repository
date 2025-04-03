c445e2ff25e652c0f66d33caa1a96679b652eeac
Zachariah Mayfield <zachariah.mayfield@outlook.com>
2025-04-03 11:11:26 -0500

    Update Get-CMD_ProcessOutput.ps1

--- Windows_Server/PowerShell/Get-CMD_ProcessOutput.ps1
+++ Windows_Server/PowerShell/Get-CMD_ProcessOutput.ps1
@@ -19,3 +19,18 @@ Function Get-CMD_ProcessOutput {
   $Output | Add-Member -type NoteProperty -name StandardOutput -Value $StandardOutput
   return $Output
 }
+
+#  EXAMPLE of Function Get-CMD_ProcessOutput 
+# Run Tableau Backup
+
+$TSM-FileName = "C:\Program Files\Tableau\Tableau Server\<version>\bin\tsm.cmd"
+
+$TSM_Maintenance_Clanup_Arguments = "maintenance clanup -1 -t -r -q -ic --logfiles-retention 7 --http-requests-table-retention 60"
+$TSM_Settings_Export_Arguments = "settings export -output-config-file C:\Tableau\Backup\"$env:Hostname"_SettingsConfigBackup_"$Get-Date".json"
+$TSM_Maintenance_Backup = "maintenance backup --file C:\Tableau\Backup\"$env:Hostname"_RepositoryFileStoreBackup_"$Get-Date"
+
+Get-CMD_ProcessOutput $TSM-FileName -FileName -Args $TSM_Maintenance_Clanup_Arguments
+
+Get-CMD_ProcessOutput $TSM-FileName -FileName -Args $TSM_Settings_Export_Arguments
+
+Get-CMD_ProcessOutput $TSM-FileName -FileName -Args $TSM_Maintenance_Backup
