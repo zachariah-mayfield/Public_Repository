a1ac5a561294f6373af609ae30d2b4723752ab21
Zachariah Mayfield <zachariah.mayfield@outlook.com>
2025-04-07 16:55:48 -0500

    Update Get-Tableau_API_Token.ps1

--- Tableau/PowerShell/Get-Tableau_API_Token.ps1
+++ Tableau/PowerShell/Get-Tableau_API_Token.ps1
@@ -1,73 +1,70 @@
-Set-ExecutionPolicy RemoteSigned -force
+import requests
+import xml.etree.ElementTree as ET
+import logging
+from datetime import datetime
 
-IF (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.Windows.Identity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  $Arguments = "& '" + $myinvocation.mycommand.definition + "'"
-  Start-Process powershell -Verb runAs -ArgumentList $Arguments
-  Break
-}
+# Setup logging
+log_file = fr"C:\Logs\Tableau_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.info("Script started")
 
-Start-Transcript -Path "C:\Logs\Tableau_$(Get-Date).log"
+# Function to check if running as admin (in Windows)
+def is_admin():
+    import ctypes
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
-$Tableau_API_UserName = CyberArk_UserName
-$Tableau_API_Password = CyberArk_Password
-$TableauServerName = "Your-Company-Tableau-Server-Name"
-$Environment = 'Development'
+if not is_admin():
+    logging.info("Restarting with admin privileges...")
+    import sys
+    import os
+    script = os.path.abspath(sys.argv[0])
+    params = " ".join([f'"{arg}"' for arg in sys.argv[1:]])
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f'"{script}" {params}', None, 1)
+    sys.exit()
 
-#region Function Get-Tableau_API_Token
-Function Get-Tableau_API_Token {
-  [CmdletBinding()]
-  Param (
-    # $Tableau_API_UserName
-    [Parameter(Mandatory=$true)
-    [string]$Tableau_API_UserName,
-    # $Tableau_API_Password
-    [Parameter(Mandatory=$true)
-    [string]$Tableau_API_Password,
-    # $TableauServerName
-    [Parameter(Mandatory=$true)
-    [string]$TableauServerName,
-    # $Environment
-    [Parameter(Mandatory=$true)]
-    [ValidateSet('Development', 'UAT', 'Production')]
-    [string]$Environment,
-    # $TableauServerAPI_Version
-    [Parameter(Mandatory=$true)
-    [string]$TableauServerAPI_Version
-  )# END Param
-  Begin {
-    # Set the Security Protocol Type
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  }# END Begin
-  Process {
-    Try {
-#region Tableau API Token
-      # Headers
-      $Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-      $Headers.Add("Content-Type", "application/xml")
-      # Body
-      $Body = "<tsRequest>
-      `n    <credentials name=`"$($Tableau_API_UserName)`" password=`"$($Tableau_API_Password)`">
-      `n    <site contentUrl=`"`" />
-      `n    </credentials>
-      `n</tsRequest>"
-      # Tableau Server API Call
-      $Tableau_Auth_Token_URL = "https://$($TableauServerName).$($Environment).Company-Domain.com/api/$($TableauServerAPI_Version)/auth/signin/"
-      $Tableau_API_Token = ((Invoke-RestMethod $Tableau_Auth_Token_URL -Method 'POST' -Headers $Headers -Body $Body -ErrorAction Stop).tsResponse.Credentials.token)
-      $Tableau_API_Token
-#endregion Tableau API Token      
-    }# END TRY
-    Catch {
-      IF ($null -ne $Error[0].Exception.Message) {
-        $Error_Exception = ($_.Exception | Select *)
-        $Error_Exception
-      }# END IF
-      $LASTEXITCODE
-      Stop-Transcript
-      EXIT $LASTEXITCODE
-    }# END Catch
-  }# END Process
-  End {}
-}# END Function Get-Tableau_API_Token
-#endregion Function Get-Tableau_API_Token
+# Example values (replace with actual logic for credentials)
+Tableau_API_UserName = "CyberArk_UserName"
+Tableau_API_Password = "CyberArk_Password"
+TableauServerName = "Your-Company-Tableau-Server-Name"
+Environment = "Development"
+TableauServerAPI_Version = "3.18"
 
-Stop-Transcript
+# Function to get Tableau API Token
+def get_tableau_api_token(username, password, server_name, environment, api_version):
+    try:
+        headers = {
+            "Content-Type": "application/xml"
+        }
+        
+        body = f"""
+        <tsRequest>
+            <credentials name="{username}" password="{password}">
+                <site contentUrl="" />
+            </credentials>
+        </tsRequest>
+        """
+
+        url = f"https://{server_name}.{environment}.Company-Domain.com/api/{api_version}/auth/signin/"
+        
+        # Make the POST request to the Tableau server
+        response = requests.post(url, headers=headers, data=body)
+        response.raise_for_status()  # Will raise an exception for bad status codes
+        
+        # Parse the XML response to get the auth token
+        response_xml = ET.ElementTree(ET.fromstring(response.text))
+        root = response_xml.getroot()
+
+        token = root.find('.//credentials/token').text
+        logging.info(f"Tableau API Token: {token}")
+        return token
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        raise
+
+# Get the Tableau API token
+token = get_tableau_api_token(Tableau_API_UserName, Tableau_API_Password, TableauServerName, Environment, TableauServerAPI_Version)
+logging.info(f"API Token: {token}")
